93b5015cb1673f1d0465aab7ab4a03c955e9b0a6
jiaweizhong66 <jiaweicurryzhong@gmail.com>
2021-07-11 16:45:43 -0400

    update

--- titanic.py
+++ titanic.py
@@ -30,6 +30,10 @@ df_train.groupby(by = "Pclass")['Fare'].mean()
 plt.figure(figsize=(40,15))
 sb.countplot(x="Age",data=df_train,hue="Survived")
 
+# heatmap
+plt.figure(figsize=(20,10))
+sb.heatmap(df_train.corr())
+
 # What is the most frequently occuring age for male and female? 
 is_f = df['Sex'] == 'female'
 df_f = df[is_f]
